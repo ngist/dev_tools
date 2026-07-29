6349ae1822e7a8886ce76ac01122156f24f6cd6d
Nat Gist <25966140+ngist@users.noreply.github.com>
2026-07-29 20:44:00 +0000

    Create install.sh

---
+++ auto_shutdown/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Installs the auto_shutdown.sh script to /opt and installs the associated systemctl service and timer.
+src_dir=`dirname "$0"`
+
+mkdir -p /opt
+cp $src_dir/auto_shutdown.sh /opt/auto_shutdown.sh
+cp $src_dir/auto_shutdown.service /etc/systemd/system/auto_shutdown.service
+cp $src_dir/auto_shutdown.timer /etc/systemd/system/auto_shutdown.timer
+
+systemctl daemon reload
+systemctl enable auto_shutdown.service
+systemctl enable auto_shutdown.timer
