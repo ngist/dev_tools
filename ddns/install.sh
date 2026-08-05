44130a6355798db0743c0671f3d981e4e5938c20
Nat Gist <nat.gist@gmail.com>
2026-08-05 18:33:41 +0000

    Add execute permission and make install robust.

--- ddns/install.sh
+++ ddns/install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-cp /tmp/dev_tools/ddns/ddns.sh /opt/ddns.sh
-cp /tmp/dev_tools/ddns/ddns.service /etc/systemd/system/ddns.service
+
+src_dir=`dirname "$0"`
+
+mkdir -p /opt
+cp $src_dir/ddns.sh /opt/ddns.sh
+cp $src_dir/ddns.service /etc/systemd/system/ddns.service
+
 systemctl daemon-reload
 systemctl enable ddns.service
 systemctl start ddns.service
