0da46518dabe81b2a1519ac801c105566783806e
Nat <nat.gist@gmail.com>
2026-08-06 16:10:19 +0000

    organize and comment

--- auto_shutdown/auto_shutdown.sh
+++ auto_shutdown/auto_shutdown.sh
@@ -9,28 +9,33 @@ if [ -z $user ]; then
     exit 1
 fi
 
-time="$(date --date='1 hour ago' '+%Y-%m-%d %H:%M:%S')"
-was_logged_in_hour_ago=$(last --present "$time" | grep $user)
-has_logged_in_within_last_hour=$(last --since "$time" | grep $user)
-booted_within_last_hour=$(last --since "$time" | grep reboot)
-active_vscode_session=$(lsof -i | grep $user | grep ESTABLISHED)
-last_activity_vscode=$(du -sh --time ~/.vscode-server | cut -f 2 | date +%s)
+hour_ago="$(date --date='1 hour ago' '+%Y-%m-%d %H:%M:%S')"
 timestamp_hour_ago=$(date --date='1 hour ago' +%s)
 
+# Check for recent boot
+booted_within_last_hour=$(last --since "$hour_ago" | grep reboot)
 
-if test "$was_logged_in_hour_ago" != ""; then
+# Checks for ssh sessions
+was_logged_in_hour_ago=$(last --present "$hour_ago" | grep $user)
+has_logged_in_within_last_hour=$(last --since "$hour_ago" | grep $user)
+
+# Checks for vscode sessions
+active_vscode_session=$(lsof -i | grep $user | grep ESTABLISHED)
+last_activity_vscode=$(du -sh --time ~/.vscode-server | cut -f 2 | date +%s)
+
+if test "$was_logged_in_hour_ago"; then
     echo $was_logged_in_hour_ago
     echo "User was logged in an hour ago or less... staying alive"
-elif test "$has_logged_in_within_last_hour" != ""; then
+elif test "$has_logged_in_within_last_hour"; then
     echo $has_logged_in_within_last_hour
     echo "User logged in within the last hour... staying alive"
-elif test "$active_vscode_session" != ""; then
+elif test "$active_vscode_session"; then
     echo $active_vscode_session
     echo "Active VSCODE session detected... staying alive"
 elif test "$last_activity_vscode" > "$timestamp_hour_ago"; then
     echo "$last_activity_vscode > $timestamp_hour_ago"
     echo "VSCODE session detected within last hour... staying alive"
-elif test "$booted_within_last_hour" != ""; then
+elif test "$booted_within_last_hour"; then
     echo $booted_within_last_hour
     echo "Instance booted within the last hour... staying alive"
 else
