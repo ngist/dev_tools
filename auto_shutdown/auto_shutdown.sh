e78677cc78f9b524907b093d6dd199924bf4c3c2
Nat <nat.gist@gmail.com>
2026-08-06 16:03:24 +0000

    Add checks for vscode sessions since it doesn't show up as a terminal login.

--- auto_shutdown/auto_shutdown.sh
+++ auto_shutdown/auto_shutdown.sh
@@ -13,6 +13,10 @@ time="$(date --date='1 hour ago' '+%Y-%m-%d %H:%M:%S')"
 was_logged_in_hour_ago=$(last --present "$time" | grep $user)
 has_logged_in_within_last_hour=$(last --since "$time" | grep $user)
 booted_within_last_hour=$(last --since "$time" | grep reboot)
+active_vscode_session=$(lsof -i | grep $user | grep ESTABLISHED)
+last_activity_vscode=$(du -sh --time ~/.vscode-server | cut -f 2 | date +%s)
+timestamp_hour_ago=$(date --date='1 hour ago' +%s)
+
 
 if test "$was_logged_in_hour_ago" != ""; then
     echo $was_logged_in_hour_ago
@@ -20,6 +24,12 @@ if test "$was_logged_in_hour_ago" != ""; then
 elif test "$has_logged_in_within_last_hour" != ""; then
     echo $has_logged_in_within_last_hour
     echo "User logged in within the last hour... staying alive"
+elif test "$active_vscode_session" != ""; then
+    echo $active_vscode_session
+    echo "Active VSCODE session detected... staying alive"
+elif test "$last_activity_vscode" > "$timestamp_hour_ago"; then
+    echo "$last_activity_vscode > $timestamp_hour_ago"
+    echo "VSCODE session detected within last hour... staying alive"
 elif test "$booted_within_last_hour" != ""; then
     echo $booted_within_last_hour
     echo "Instance booted within the last hour... staying alive"
